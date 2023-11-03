f01dbb33b5532dfb27d6ba35349f07df9fc2fba3
kategera <kate-5g@bk.ru>
2023-11-04 01:43:35 +0300

    Task 1-5

---
+++ scripts/hw5.js
@@ -0,0 +1,69 @@
+// //Задание 1
+// let firstNum = Number (prompt("Напишите первое число"));
+// let secondNum = Number (prompt("Напишите первое число"));
+
+// function compare(firstNum, secondNum) {
+//     if (firstNum < secondNum) {
+//         return firstNum;
+//     } else {
+//         return secondNum;
+//     }
+// }
+
+// console.log(compare(firstNum, secondNum));
+
+// //Задание 2
+// let numTask2 = Number (prompt("Какое число нужно проверить?"));
+
+// function checkEven(numTask2) {
+//     if (numTask2% 2 === 0) {
+//         return "Число четное";
+//     } else {
+//         return "Число нечетное";
+//     }
+// }
+// alert(checkEven(numTask2));
+
+// //Задание 3.1
+// let numTask3 = Number (prompt("Какое число нужно возвести в крадрат?"));
+
+// function square(numTask3) {
+//     console.log(numTask3 ** 2);
+// }
+
+// square(numTask3);
+
+// //Задание 3.2
+// let numTask3 = Number (prompt("Какое число нужно возвести в крадрат?"));
+
+// function square(numTask3) {
+//     return numTask3 ** 2;
+// }
+
+// alert(square(numTask3));
+
+// //Задание 5
+// "use strict"
+// let age = Number(prompt("Сколько Вам лет?"));
+// let printMessage;
+
+
+// if (age < 0) {
+//     printMessage = function() {
+//         alert("Вы ввели неправильное значение");
+//     }
+
+// } else if (age <= 12) {
+//     printMessage = function() {
+//         alert("Привет, друг!");
+//     }
+
+// } else {
+//     printMessage = function() {
+//         alert("Добро пожаловать!");
+//     }
+// }
+
+// printMessage();
+
+//Задание 6
